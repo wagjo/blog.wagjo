a9bdce92b81423c0b3b62a77ae6e5cadc444e315
Jozef Wagner <jozef.wagner@gmail.com>
2015-04-01 12:11:04 +0200

    initial

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject org.wagjo/blog.wagjo "0.1.0-SNAPSHOT"
+  :description "blog.wagjo.com documentation generation."
+  :url "https://github.com/wagjo/blog.wagjo"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.dunaj/dunaj.doc "0.1.0-SNAPSHOT"]
+                 [org.asciidoctor/asciidoctorj "1.5.2"]])
---
+++ src/wagjo/blog/docgen.clj
@@ -0,0 +1,54 @@
+;; Copyright (C) 2015, Jozef Wagner. All rights reserved.
+;;
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 1.0
+;; (http://opensource.org/licenses/eclipse-1.0.php) which can be
+;; found in the file epl-v10.html at the root of this distribution.
+;;
+;; By using this software in any fashion, you are agreeing to be bound
+;; by the terms of this license.
+;;
+;; You must not remove this notice, or any other, from this software.
+
+(ns wagjo.blog.docgen
+  "Facilities for generating documentation."
+  (:api dunaj)
+  (:require [dunaj.doc :refer [gen-doc]]
+            [dunaj.repl]))
+
+(def blog-config
+  {:current-version nil
+   :target-path "gh-pages"
+   :static-path "blog"
+   :logo-url "logo.png"
+   :proj-name "Jozef Wagner"
+   :no-doc-title true
+   :disqus "wagjo"
+   :proj-url "http://blog.wagjo.com/"
+   :copy-years "2015,"
+   :additional-copyright
+   "2008, 2015, Rich Hickey and Clojure contributors"
+   :authors ["Jozef Wagner"]
+   :header-menu
+   [{:url "http://wagjo.com"
+     :name "Homepage" :icon :fa-home}
+    {:url "http://blog.wagjo.com"
+     :name "Blog" :icon :fa-pencil}
+    {:url "http://blog.wagjo.com/feed.xml"
+     :name "RSS Feed" :icon :fa-rss}]
+   :static-pages
+   [{:filename "index" :name "Blog"}
+    {:filename "set" :name "Universal and Complement Sets in Dunaj"
+     :disqus-id "set"}]})
+
+
+;;;; Scratch
+
+(scratch []
+
+  []
+
+  (dunaj.doc/gen-static blog-config)
+
+
+)
